bf16a45c830693d811874b595d3efed23bf7d4ae
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-03-24 14:24:59 -0700

    all three services render to page

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const axios = require("axios");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/photogallery", async (req, res) => {
+  await axios
+    .get(`http://localhost:3001/photogallery`)
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      if (err) throw err;
+    });
+});
+
+app.get("/api/reviews/:locationId", async (req, res) => {
+  const { locationId } = req.params;
+  await axios
+    .get(`http://localhost:3000/api/reviews/${locationId}`)
+    .then(result => {
+      res.send(result.data);
+    });
+  res.end();
+});
+
+app.get("/api/reserve/:locationId", async (req, res) => {
+  const { locationId } = req.params;
+  await axios
+    .get(`http://localhost:3002/api/reserve/${locationId}`)
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      if (err) throw err;
+    });
+  res.end();
+});
+
+app.get("/api/reserve/dates/:check:out", async (req, res) => {
+  const { out } = req.params;
+  await axios
+    .get(`http://localhost:3002/api/reserve/dates/${out}`)
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      if (err) throw err;
+    });
+});
+
+app.listen(8080, () => {
+  console.log("Listening on 8080");
+});
